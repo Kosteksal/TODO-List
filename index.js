35373ad7e2016094306b3c4d9a967adcd6b40427
Kosteksal <kosteks@outlook.com>
2022-08-09 12:00:24 +0300

    feat: add main layout

--- index.js
+++ index.js
@@ -18,4 +18,31 @@ let dataArr = [
 function builder () {
     const container = document.createElement('div');
     container.classList.add('container');
+    const controls = document.createElement('div');
+    controls.classList.add('controls');
+
+    const btnContainer = document.createElement('div');
+    btnContainer.classList.add('btn-container');
+    const mainTitle = document.createElement('h1');
+    mainTitle.innerText = 'TODO List';
+
+    const wrapper = document.createElement('div');
+    wrapper.classList.add('wrapper');
+    const listContainer = document.createElement('div');
+    listContainer.classList.add('list-container');
+    const noteContainer = document.createElement('div');
+    noteContainer.classList.add('note-container');
+
+
+
+    BODY.append(container);
+    container.append(controls);
+    container.append(wrapper);
+    wrapper.append(listContainer);
+    wrapper.append(noteContainer);
+    controls.append(mainTitle);
+    controls.append(btnContainer);
 }
+
+
+document.addEventListener('DOMContentLoaded', builder);
